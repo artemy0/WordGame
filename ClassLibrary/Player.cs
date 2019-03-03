82b012955b76a0919d96fb0fbd6f8694f5063f5b
Artem Gorbachev <gorbachev99artem@gmail.com>
2019-03-03 22:25:40 +0300

    WordGameBetaTest 1.0

---
+++ ClassLibrary/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Player
+    {
+        private static int Count = 1;
+        public string Name { get; private set; }
+        public int Id { get; private set; }
+        public int Points { get; private set; } = 0;
+
+        public Player(string name)
+        {
+            Name = name;
+            Id = Count++;
+        }
+
+        public void Move(string word)
+        {
+            Console.Write($"The move of the {Id} player named {Name}: ");
+            string enteredWords = Console.ReadLine();
+
+            string tempWord = word;
+
+            foreach (char character in enteredWords)
+            {
+                int index = tempWord.IndexOf(character);
+                if (index != -1)
+                    tempWord = tempWord.Remove(index, 1);
+                else
+                {
+                    Console.WriteLine("You have entered an invalid or non-existent word!");
+                    throw new Exception($"The {Id} player named {Name} lost");
+                }
+            }
+
+            Console.WriteLine("That's right. Press any button to move to the next player.");
+        }
+    }
+}
---
+++ WordGame/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary;
+using System.Collections;
+using System.Collections.Generic;
+using PagedList;
+
+namespace WordGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello. This is a word game. press any key to start.");
+            Console.ReadKey();
+            Console.Clear();
+
+            string Word;
+
+            while (true)
+            {
+                Console.Write("Enter a word from 8 to 20 characters from which you will need to make other words: ");
+                Word = Console.ReadLine();
+                if (Word.Length >= 8 && Word.Length <= 20)
+                    break;
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("You have entered recent words. Try again.");
+                    continue;
+                }
+            }
+
+            PrintWord(Word);
+
+            Console.Write("Enter a name for the first player: ");
+            Player player1 = new Player(Console.ReadLine());
+
+            Console.Write("Enter a name for the second player: ");
+            Player player2 = new Player(Console.ReadLine());
+
+            var players = new MoreCollections.CircularList<Player>(new Player[] { player1, player2 });
+            int index = 0;
+            try
+            {
+                do
+                {
+                    PrintWord(Word);
+
+                    players[index++].Move(Word);
+
+                } while (true);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine($"Thanks for playing.\n{player1.Name} has {player1.Points} points\n{player2.Name} has {player2.Points} points.");
+            }
+
+            Console.ReadKey();
+        }
+
+        public static void PrintWord(string word)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Source word is " + word.ToUpper());
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+    }
+}
